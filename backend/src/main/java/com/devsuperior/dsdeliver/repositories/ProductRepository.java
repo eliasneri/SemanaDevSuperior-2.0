29f0f2f0195637c079eb7afd221f47d96b21cffc
eliasneri <eliasneri@hotmail.com>
2021-01-04 23:07:29 -0300

    Repositories

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.devsuperior.dsdeliver.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devsuperior.dsdeliver.entities.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
