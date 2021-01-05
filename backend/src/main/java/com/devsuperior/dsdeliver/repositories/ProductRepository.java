66812595bb13f63736b595ad27f95a20fa16f90e
eliasneri <eliasneri@hotmail.com>
2021-01-05 11:34:44 -0300

    GET /products

--- backend/src/main/java/com/devsuperior/dsdeliver/repositories/ProductRepository.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/repositories/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dsdeliver.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.devsuperior.dsdeliver.entities.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>{
+	
+	List<Product> findAllByOrderByNameAsc();
 
 }
