f80f518a162b4dd74b2953ab1045c762aa6376c8
eliasneri <eliasneri@hotmail.com>
2021-01-06 17:07:17 -0300

    Add Footer

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,15 +2,12 @@ import './App.css';
 
 import Navbar from './Navbar';
 import Home from './Home';
-import Footer from './Footer';
 
 function App() {
   return (
     <>
-    <Navbar />
-    <Home />
-    <Footer />
-
+      <Navbar />
+      <Home />
    </> 
   );
 }
--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -1,12 +1,26 @@
 import './styles.css'
+import { ReactComponent as YouTubeImage } from './youtube.svg';
+import { ReactComponent as LinkDin } from './linkedin.svg';
+import { ReactComponent as Insta } from './instagram.svg';
 
 function Footer() {
     return (
-        <div className="main-footer">
+        <footer className="main-footer">
+            App desenvolvido durante a 2ª ed. do evento Semana DevSuperior 
+             
             <div className="footer-icons">
-
+                <a href="https://www.youtube.com/c/DevSuperior" target="_new">
+                    <YouTubeImage />
+                </a>
+                <a href="https://www.linkedin.com/school/devsuperior" target="_new">
+                    <LinkDin />
+                </a>
+                <a href="https://www.instagram.com/devsuperior.ig/" target="_new">
+                <Insta />
+                </a>
+                
             </div>
-        </div>
+        </footer>
     )
 }
 
