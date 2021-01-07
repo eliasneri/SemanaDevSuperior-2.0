2c590f9bc8d7a1c1a86117ec87886a32f1fd0e07
eliasneri <eliasneri@hotmail.com>
2021-01-07 11:50:49 -0300

    Add Routes and Steps

--- front-web/src/Navbar/index.tsx
+++ front-web/src/Navbar/index.tsx
@@ -1,11 +1,13 @@
 import './styles.css'
 import {ReactComponent as Logo } from './logo.svg'
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
         <nav className="main-navbar">
-            <a href="home"><Logo /></a>
-            <a href="home" className="logo-text">DS Delivery</a>
+            <Logo />
+            <Link to="/" className="logo-text">DS Delivery</Link>
+
         </nav>
     )
 }
